6698b8d06ae9038ad9af17116c9b089b7ed40905
Victor Um <victorum692003@gmail.com>
2023-07-22 23:19:43 -0700

    - created CheckOut.java file - created a panel and label - was able to show a green background and it says "Check-Out"

---
+++ CheckOut.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CheckOut extends JFrame {
+
+    CheckOut(){
+        JPanel panel = new JPanel();
+        panel.setBounds(5, 5, 790, 390);
+        panel.setBackground(new Color(3,45,48));
+        panel.setLayout(null);
+        add(panel);
+
+        JLabel label = new JLabel("Check-Out");
+        label.setBounds(100, 20, 100, 30);
+        label.setFont(new Font ("Times New Roman", Font.PLAIN, 20));
+        label.setForeground(Color.WHITE);
+        panel.add(label);
+
+        setLayout(null);
+        setSize(800, 400);
+        setLocation(500, 210);
+        setVisible(true);
+
+    }
+
+    public static void main(String [] args){
+        new CheckOut();
+
+    }
+}
