d66c00e97cedb3769e2f23439cec2ad4755bab4b
Victor Um <victorum692003@gmail.com>
2023-08-01 19:46:12 -0700

    updating all checking in and in n out

--- checkin.js
+++ checkin.js
@@ -1,13 +1,49 @@
-// Function to handle check-in
-document.getElementById("checkinForm").addEventListener("submit", function (event) {
+document.getElementById('checkinForm').addEventListener('submit', function (event) {
   event.preventDefault();
-  const checkinName = document.getElementById("checkinName").value;
-  if (checkinName.trim() !== "") {
-    // Here you can perform any additional actions like sending data to the server
-    // or storing it in a database.
-    document.getElementById("checkinForm").style.display = "none";
-    document.getElementById("message").innerText = `Welcome, ${checkinName}!`;
-  } else {
-    alert("Please enter your name for check-in.");
+
+  const roomNumber = document.getElementById('roomNumber').value;
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+
+  // Check if the date is in the past.
+  const currentDate = new Date();
+  const selectedDate = new Date(date);
+  if (selectedDate < currentDate) {
+    document.getElementById('message').innerHTML = '<p>Check-in was unsuccessful. The selected date is in the past.</p>';
+    return;
   }
+
+  // Check if the room number contains only numbers.
+  if (!/^\d+$/.test(roomNumber)) {
+    document.getElementById('message').innerHTML = '<p>Check-in was unsuccessful. Room number should contain only numbers.</p>';
+    return;
+  }
+
+  // Create a JSON object with the form data.
+  const formData = {
+    roomNumber,
+    date,
+    time,
+    firstName,
+    lastName,
+  };
+
+  // Send a POST request to the server to save the check-in data.
+  fetch('/checkin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.text())
+    .then((message) => {
+      document.getElementById('message').innerHTML = `<p>${message}</p>`;
+    })
+    .catch((error) => {
+      console.error('Error saving check-in:', error);
+      document.getElementById('message').innerHTML = '<p>Error saving check-in.</p>';
+    });
 });
