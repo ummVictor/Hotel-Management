5ff1c8bfb0947ea57048eb77a7684cdfbf91f8f5
Victor Um <victorum692003@gmail.com>
2023-08-09 19:16:01 -0700

    updating check in

--- check-in.js
+++ check-in.js
@@ -1,14 +1,32 @@
-// Function to handle check-in
-document.getElementById("checkinForm").addEventListener("submit", function (event) {
+document.addEventListener("DOMContentLoaded", function() {
+  const checkinForm = document.getElementById("checkinForm");
+  const messageDiv = document.getElementById("message");
+
+  checkinForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    const checkinName = document.getElementById("checkinName").value;
-    if (checkinName.trim() !== "") {
-      // Here you can perform any additional actions like sending data to the server
-      // or storing it in a database.
-      document.getElementById("checkinForm").style.display = "none";
-      document.getElementById("message").innerText = `Welcome, ${checkinName}!`;
+
+    const roomNumber = document.getElementById("roomNumber").value;
+    const date = document.getElementById("date").value;
+    const militaryTime = document.getElementById("time").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+
+    const currentTime = new Date();
+    const selectedTime = new Date(`${date}T${militaryTime}`);
+    const selectedDate = selectedTime.toDateString(); // Get the selected date as a string
+
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+    const formattedTime = selectedTime.toLocaleTimeString('en-US', options);
+
+    const capitalizedFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+    const capitalizedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+    if (currentTime.toDateString() !== selectedDate) {
+      messageDiv.innerHTML = `Welcome, ${capitalizedFirstName} ${capitalizedLastName}! Your reservation is not today. Please make sure to come on your reservation date.`;
+    } else if (currentTime >= selectedTime) {
+      messageDiv.innerHTML = `Welcome, ${capitalizedFirstName} ${capitalizedLastName}! Your room, ${roomNumber}, is not available. Your room will be ready after ${formattedTime}.`;
     } else {
-      alert("Please enter your name for check-in.");
+      messageDiv.innerHTML = `Welcome, ${capitalizedFirstName} ${capitalizedLastName}! Your room, ${roomNumber}, has been available since ${formattedTime}. Feel free to walk into your room. Any further questions, you can ask the hotel front desk.`;
     }
   });
-  
+});
