b5ef6be4d5420cf671713de0b80c13b6bc0eea2f
brian023rojas <brian.rojas.023@my.csun.edu>
2023-08-14 20:27:58 -0700

    in

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+// Use body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set the views directory and view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import routes
+const routes = require('./routes/index');
+app.use('/', routes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
