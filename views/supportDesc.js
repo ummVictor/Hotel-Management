5fb324bde7a6d5579aa17b81b5f4a3bcec09c00f
brian023rojas <brian.rojas.023@my.csun.edu>
2023-08-15 20:08:13 -0700

    ready

---
+++ views/supportDesc.js
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Support</title>
+  <!-- Link to external stylesheet -->
+  <link rel="stylesheet" type="text/css" href="/styles.css">
+  <style>
+    /* Styling for the body element */
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f7f7f7;
+      text-align: center;
+    }
+    /* Styling for the main content container */
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #fff;
+      border-radius: 10px;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    }
+    /* Styling for the main heading */
+    h1 {
+      color: #333;
+      margin-bottom: 20px;
+    }
+    /* Styling for paragraphs */
+    p {
+      margin-bottom: 20px;
+      font-size: 18px;
+      color: #555;
+    }
+    /* Styling for unordered list */
+    ul {
+      text-align: left;
+      list-style-type: none;
+      padding-left: 0;
+    }
+    /* Styling for list items */
+    li {
+      margin-bottom: 10px;
+    }
+    /* Styling for links */
+    a {
+      color: #007bff;
+      text-decoration: none;
+      transition: color 0.3s ease;
+    }
+    /* Styling for links on hover */
+    a:hover {
+      color: #0056b3;
+    }
+    /* Styling for the image container */
+    .image-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 30px;
+    }
+    /* Styling for images */
+    .image {
+      max-width: 100%;
+      border-radius: 10px;
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    }
+  </style>
+</head>
+<body>
+  <!-- Main content container -->
+  <div class="container">
+    <!-- Main heading -->
+    <h1>Support</h1>
+    <!-- Paragraph about support -->
+    <p>Need help? We're here for you! Our goal is to make your stay comfortable and hassle-free.</p>
+    <!-- Unordered list of support options -->
+    <ul>
+      <li><a href="/">View Occupied Rooms</a> - Check out our guests and see who's staying with us.</li>
+      <li><a href="/checkin">Check In</a> - Ready to explore our rooms? Start your journey here.</li>
+      <li><a href="/checkout">Check Out</a> - Time to say goodbye? Use this link for a smooth departure.</li>
+    </ul>
+    <!-- Paragraph about contacting the front desk -->
+    <p>For any inquiries or assistance, our front desk is available 24/7. Feel free to <a href="tel:+1234567890">call us</a> anytime!</p>
+    <!-- Container for displaying an image -->
+    <div class="image-container">
+      <!-- Image of friendly hotel staff -->
+      <img class="image" src="/staff.jpg" alt="Friendly Hotel Staff">
+    </div>
+  </div>
+</body>
+</html>
